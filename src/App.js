ebe14e30bcfac3778e5ca915976ebc3a73d65dcd
Yannick <yannick.rincon.9985@miremington.edu.co>
2023-08-19 18:03:02 -0500

    cambiodenombresComponentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Heade from './componenetes/comp1/comp1';
 import Foott from './componenetes/comp2/comp1';
+import Location from './componenetes/comp3/comp1';
+import Carticon from './componenetes/comp4/comp1';
+import Description from './componenetes/comp5/comp1';
+import Main from './componenetes/comp6/comp1';
+import Cart from './componenetes/comp7/comp1';
+import Contact from './componenetes/comp8/comp1';
+
 function App() {
   return (
     <>
     
     <div className="App">
     <div><Heade></Heade></div>
-      <header className="App-header">
-      </header>
+    <div><Carticon></Carticon></div>
+    <div><Description></Description></div>
+    <div><Main></Main></div>
+    <div><Cart></Cart></div>
+    <div><Contact></Contact></div>
+    <div><Location></Location></div>
     <div><Foott></Foott></div>
     </div>
     
---
+++ src/componenetes/comp7/comp7.js
@@ -0,0 +1,5 @@
+function Location(){
+    const name='Yannick'
+    return <h1>Hellso {name}</h1>;
+}
+export default Location;
