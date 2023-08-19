3b247d20857183c82388a1e37477c3a62faba551
Yannick <yannick.rincon.9985@miremington.edu.co>
2023-08-19 11:05:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Heade from './componenetes/comp1/comp1';
+
+function App() {
+  return (
+    <><div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+    <div><Heade></Heade></div></>
+  );
+}
+
+export default App;
---
+++ src/componenetes/comp1/comp1.js
@@ -0,0 +1,7 @@
+
+
+function Heade(){
+    return <h1>Hello World</h1>;
+}
+
+export default Heade;
